3edf7e5e01bd3b248b436721a0ded064fc7554de
berkanioussama <berkanioussama@gmail.com>
2024-01-11 22:30:08 +0100

    add oauth login google and github

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,22 @@
 import type { NextAuthConfig } from "next-auth"
-import bcrypt from 'bcryptjs'
 import credentials from "next-auth/providers/credentials"
+import GitHub  from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
+import bcrypt from 'bcryptjs'
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/user"
 
 export default {
   providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
     credentials({
       async authorize(credentials){
         const validatedFields = LoginSchema.safeParse(credentials)
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,17 +1,26 @@
 'use client'
 
+import { signIn } from 'next-auth/react';
 import {FcGoogle} from 'react-icons/fc'
 import {FaGithub} from 'react-icons/fa'
 import { Button } from '@/components/ui/button';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 const Social = () => {
+
+    const handleClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT
+        })
+    }
+
     return (
         <div className="w-full flex gap-x-2 items-center">
             <Button 
                 size="lg" 
                 variant="outline" 
                 className='w-full' 
-                onClick={()=>{}}
+                onClick={()=>handleClick("google")}
             >
                 <FcGoogle className="w-5 h-5"/>
             </Button>
@@ -19,7 +28,7 @@ const Social = () => {
                 size="lg" 
                 variant="outline" 
                 className='w-full' 
-                onClick={()=>{}}
+                onClick={()=>handleClick("github")}
             >
                 <FaGithub className="w-5 h-5"/>
             </Button>
